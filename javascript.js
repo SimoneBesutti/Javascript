9228012606c278606a92fe4c38cbf99c2a8374db
simo <simonebesutti18@gmail.com>
2019-07-09 11:45:16 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+alert('ciao, benvenuto')
+
+var nome, cognome, colorepreferito;
+nome = prompt("Inserisci il tuo nome");
+
+cognome = prompt("Inserisci il tuo cognome");
+
+colorepreferito = prompt("Inserisci il tuo colore preferito")
+
+risultato = nome + cognome + colorepreferito + 19;
+
+document.writeln(risultato);
